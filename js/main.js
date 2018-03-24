00f8319c058f7e586c6de4b3729a2a210eefc4be
Sagiv Harel <you@example.com>
2018-03-24 17:56:00 +0300

    simple file uploader

---
+++ js/main.js
@@ -0,0 +1,8 @@
+Dropzone.options.imgUploader = {
+    paramName: "uploadedImg",
+    url: 'upload_img.php', // The name that will be used to transfer the file
+    maxFilesize: 5, // MB
+    dictDefaultMessage: 'גרור לכאן קבצים שברצונך להעלות' 
+}
+
+
